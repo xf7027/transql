5532a81e3ab398f4696d2bd402490eb67f3edab3
xf7027 <xf7027@sina.com>
2015-11-28 13:49:15 +0800

    0.0.4

--- insert.sql
+++ insert.sql
@@ -1,1 +1,2 @@
-insert into work_area (work_area_id,name,work_type_id,area_id,work_mode) values (%s,%s,%s,%s,%s)
+insert into work_area VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+insert into work_area VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
--- select.sql
+++ select.sql
@@ -1,1 +1,2 @@
-select work_area_id,name,work_type_id,area_id,work_mode from work_area 
+select * from work_area w where w.local_net_id='352'
+select * from work_area w where w.local_net_id='353'
